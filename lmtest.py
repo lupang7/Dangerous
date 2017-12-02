7b79409c73d237b0167fc91bc509871bb61d7348
lupang <lupang@yangqianguan.com>
2017-12-02 15:34:16 +0800

    test 滑动

--- lmtest.py
+++ lmtest.py
@@ -1,4 +1,4 @@
-#coding:utf-8
+# coding:utf-8
 """
 Simple iOS tests, showing accessing elements and getting/setting text from them.
 """
@@ -8,17 +8,18 @@ from random import randint
 from appium import webdriver
 from time import sleep
 import test
+
+
 # from PIL import Image
 # from imageCmp import Appium_Extend
 
 class SimpleIOSTests(unittest.TestCase):
-
     def setUp(self):
         # set up appium
-        app = os.path.abspath('app/loanmarket10000.app')
+        app = os.path.abspath('app/loanmarket.zip')
         self.driver = webdriver.Remote(
             command_executor='http://127.0.0.1:4723/wd/hub',
-            #真机的配置
+            # 真机的配置
             # desired_capabilities={
             #     'app': app,
             #     'platformName': 'iOS',
@@ -27,39 +28,42 @@ class SimpleIOSTests(unittest.TestCase):
             #     'udid':'24d5b977a1811d8eaaeece32b0338161544003dd',
             #     'bundleId':'com.lingyue.loanmarketT'
             # })
-            #虚拟机的配置
+            # 虚拟机的配置
             desired_capabilities={
                 'app': app,
                 'platformName': 'iOS',
-                'platformVersion': '10.2',
-                'deviceName': 'iPhone 6s',
+                'platformVersion': '11.1',
+                'deviceName': 'iPhone 7',
             })
         # self.extend = Appium_Extend(self.driver)
+
     sleep(5)
 
-    def test_get_screen_by_element(self):
-        self.driver.get_screenshot_as_file("/Users/panglu/pytest/image/img1.png")
-        # element = self.driver.find_element_by_id("welcome1")
-        #
-        # load = self.extend.load_image("/Users/panglu/pytest/image/引导页1.jpg")
-        # # 要求百分百相似
-        # result = self.extend.get_screenshot_by_element(element).same_as(load, 0)
-        # self.assertTrue(result)
-
-
-
-
-
-    # def getSize(self):
-    #     x = self.driver.get_window_size()['width']
-    #     y = self.driver.get_window_size()['height']
-    #     return (x, y)
+    # def test_get_screen_by_element(self):
+    #     self.driver.get_screenshot_as_file("/Users/panglu/pytest/image/img1.png")
+    # element = self.driver.find_element_by_id("welcome1")
     #
-    # def test_leftScroll(self):
-    #     location = self.getSize()
-    #     print("location is" ,location)
-    #     self.driver.swipe(start_x=int(location[0]), start_y=int(location[1]), end_x=int(-location[0]), end_y=int(location[1]), duration=800)
-    #     sleep(1)
+    # load = self.extend.load_image("/Users/panglu/pytest/image/引导页1.jpg")
+    # # 要求百分百相似
+    # result = self.extend.get_screenshot_by_element(element).same_as(load, 0)
+    # self.assertTrue(result)
+
+
+
+
+
+    def getSize(self):
+        x = self.driver.get_window_size()['width']
+        y = self.driver.get_window_size()['height']
+        return (x, y)
+
+    def test_leftScroll(self):
+        self.driver.tap([(250, 398)])
+        location = self.getSize()
+        for i in range(2):
+            self.driver.swipe(start_x=int(location[0]), start_y=int(location[1]/2), end_x=int(-location[0]*2),end_y=0, duration=200)
+            sleep(3)
+        self.driver.find_element_by_accessibility_id("立即使用").click()
 
 
 
