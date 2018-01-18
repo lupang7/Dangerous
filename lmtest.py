53b40c9881c16779565771d4b11654bd6b351e62
lupang <lupang@yangqianguan.com>
2018-01-18 14:36:32 +0800

    修改手机型号

--- lmtest.py
+++ lmtest.py
@@ -9,9 +9,12 @@ from time import sleep
 import time
 import imageCmp
 from PIL import Image
-import apiAll
+from apiAll import ApiRes
 import json
-import random
+import logging
+
+ApiRes =ApiRes()
+
 
 class SimpleIOSTests(unittest.TestCase):
     def setUp(self):
@@ -32,7 +35,7 @@ class SimpleIOSTests(unittest.TestCase):
             desired_capabilities={
                 'app': app,
                 'platformName': 'iOS',
-                'platformVersion': '11.1',
+                'platformVersion': '11.2',
                 'deviceName': 'iPhone 6s',
                 'noReset': 'true'
 
@@ -44,27 +47,29 @@ class SimpleIOSTests(unittest.TestCase):
         y = self.driver.get_window_size()['height']
         return (x, y)
 
+    #测试欢迎页
     def test_a_welcome(self):
         self.driver.tap([(250, 398)])
         location = self.getSize()
         for i in range(2):
             #截图对比
-            img = "/Users/panglu/pytest/image/img" + str(int(time.time())) + ".png"
+            img = "/Users/panglu/mist/Dangerous/images/img" + str(int(time.time())) + ".png"
             self.driver.get_screenshot_as_file(img)
-            image1 = Image.open("/Users/panglu/pytest/image/引导页" + str(i) + ".png")
+            image1 = Image.open("/Users/panglu/mist/Dangerous/images/引导页" + str(i) + ".png")
             image2 = Image.open(img)
-            imageCmp.classfiy_histogram_with_split(image1, image2)
+            Similarity = imageCmp.classfiy_histogram_with_split(image1, image2)
             self.driver.swipe(int(location[0]), int(location[1]/2), int(-location[0]*2),0, 200)
             sleep(3)
         self.driver.find_element_by_accessibility_id("立即使用").click()
 
+    # 登录失败获取不到元素，放弃
     # def test_b_login(self):
     #     self.driver.find_element_by_accessibility_id("userCenter").click()
     #     self.driver.find_element_
     #     self.driver.find_element_by_xpath('//XCUIElementTypeApplication[@name="六六钱庄"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeTextField[1]/XCUIElementTypeTextField').click()
     #     list = ('130', '131', '132', '133', '134', '135', '136', '137', '138', '139', '144', '147',
     #             '150', '151', '152', '153', '155', '156', '157', '158', '159', '176', '177', '178',
-    #             '180', '181', '182', '183', '184', '185', '186', '187', '188', '189',)
+    #             '180', '181', '182', '1823', '184', '185', '186', '187', '188', '189',)
     #     telNum = random.choice(list) + "".join(random.choice('0123456789') for i in range(8))
     #     for j in telNum :
     #         self.driver.find_element_by_accessibility_id("j").click()
@@ -80,25 +85,40 @@ class SimpleIOSTests(unittest.TestCase):
     #     self.driver.find_element_by_accessibility_id('ic fh').click()
 
 
-
+    #测试首页的各个模块
     def test_c_homePage(self):
-        res = apiAll.getCard()
-        rescard = json.loads(res)
-        cardNum = len(rescard['body'])
-        print(cardNum)
-        if cardNum >= 2:
+        resbanner = json.loads(ApiRes.getBanner())
+        if len(resbanner['body']) >= 1:
+            pass
+        else:
+            print("没有设置轮播图")
+        rescard = json.loads(ApiRes.getCard())
+        print(len(rescard['body']))
+        if len(rescard['body']) >= 2:
             self.driver.find_element_by_xpath ("//XCUIElementTypeApplication[@name=\"六六钱庄\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeTable/XCUIElementTypeCell[1]/XCUIElementTypeButton[1]").click()
             self.driver.find_element_by_accessibility_id("返回").click()
-
         else:
-            print("卡片为空")
+            print("没有配置卡片")
+        resscroll = json.loads(ApiRes.getScroll())
+        if len(resscroll['body']) >=1:
+            pass
+        else:
+            print("没有设置滚动条")
+
+        self.driver.find_element_by_accessibility_id("换一批").click()
+
+    #测试webview
+
 
     def test_d_allProduct(self):
         self.driver.find_element_by_accessibility_id("全部贷款").click()
 
+
     def test_e_Activity(self):
         self.driver.find_element_by_accessibility_id("更多活动").click()
 
+    def tearDown(self):
+        self.driver.quit()
 
 
 
